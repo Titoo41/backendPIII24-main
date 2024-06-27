1f3d4ad2fe3d4b2bb034cc9aeb139b65f63c35ac
titoo41 <admcoronasdelsur@gmail.com>
2024-06-27 20:16:12 -0300

    PROYECTO TERMINADO

--- src/modules/gamer/gamer.routes.js
+++ src/modules/gamer/gamer.routes.js
@@ -32,7 +32,7 @@ router.get("/api/gamer/:id",  async (req, res) => {
   }
 });
 
-// POST /api/gamer
+/* POST /api/gamer
 router.post("/api/gamer", async (req, res) => {
   // #swagger.tags = ['Gamer']
   try {
@@ -45,9 +45,35 @@ router.post("/api/gamer", async (req, res) => {
     console.log(error);
     return res.status(500).send(error);
   }
+});*/
+
+// POST /api/gamer
+router.post("/api/gamer", async (req, res) => {
+  // #swagger.tags = ['Gamer']
+  try {
+    const { name, age, game } = req.body;
+
+    const newGamer = {
+      name,
+      age,
+      game,
+    };
+    console.log(newGamer);
+
+    const gamer = await gamerService.save(newGamer);
+
+    if (!gamer) {
+      return res.status(400).send({ error: "Campos incompletos" });
+    }
+
+    return res.status(201).send(gamer);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
 });
 
-// PUT /api/gamer/:id
+/* PUT /api/gamer/:id
 router.put("/api/gamer/:id",  async (req, res) => {
   // #swagger.tags = ['Gamer']
   try {
@@ -56,6 +82,28 @@ router.put("/api/gamer/:id",  async (req, res) => {
     const user = await gamerService.update(userId, updatedUser);
     return res.status(200).send(user);
 
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});*/
+
+// PUT /api/gamer/:id
+router.put("/api/gamer/:id", async (req, res) => {
+  // #swagger.tags = ['Gamer']
+  try {
+    const userId = req.params.id;
+    const { name, age, game } = req.body;
+
+    const updatedGamer = {
+      name,
+      age,
+      game,
+    };
+    console.log(updatedGamer);
+
+    const user = await gamerService.update(userId, updatedGamer);
+    return res.status(200).send(user);
   } catch (error) {
     console.log(error);
     return res.status(500).send(error);
@@ -85,85 +133,3 @@ module.exports = router;
 
 
 
-
- //   sin tags 
-
-
-
-
-
-
-
-
- /*
-const express = require("express");
-const gamerService = require("./gamer.service");
-
-const router = express.Router();
-
-
-
-
-router.get("/api/gamer", async (req, res) => {
-  try {
-    const params = JSON.parse(req.headers['params']);
-    console.log("Parsed params:", params); // Agrega este console.log
-    const paginated = await gamerService.paginated(params);
-    return res.status(200).send(paginated);
-  } catch (error) {
-    console.log("Error:", error);
-    return res.status(500).send(error);
-  }
-});
-
-router.get("/api/gamer/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const user = await gamerService.findOneById(userId);
-    return res.status(200).send(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
-});
-
-
-router.post("/api/gamer", async (req, res) => {
- 
-  try {
-    const newUser = req.body;
-    const user = await gamerService.save(newUser);
-    return res.status(201).send(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
-});
-
-
-router.put("/api/gamer/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const updatedUser = req.body;
-    const user = await gamerService.update(userId, updatedUser);
-    return res.status(200).send(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
-});
-
-
-router.delete("/api/gamer/:id", async (req, res) => {
-  try {
-    const userId = req.params.id;
-    await gamerService.remove(userId);
-    return res.status(200).send("Usuario eliminado correctamente.");
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
-});
-
-module.exports = router;
-*/
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -32,23 +32,58 @@ router.get("/api/user/:id",  async (req, res) => {
   }
 });
 
+// prueba
+
+
+
 // POST /api/user
 router.post("/api/user", async (req, res) => {
   // #swagger.tags = ['Usuario']
+  const { firstname, lastname, email, domicilio, celular, documento, rol, area } = req.body;
+  // const newUser = req.body;
+
   try {
-    const newUser = req.body;
-    console.log(newUser);
-    const user = await userService.save(newUser);
+
+    const existingUser = await userService.findOne({ email:email });
+    if (existingUser) {
+      return res.status(400).send({ error: 'El email ya está registrado.' });
+    }
+
+
+    const newUser = {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      domicilio: domicilio,
+      celular: celular,
+      documento: documento,
+      rol: rol,
+      area: area,
+    }
+
+    // Guardar el nuevo usuario
+    const user = await userservice.save(newUser);
+
+    console.log(user)
+    if(!user){
+      return res.status(400).send({error: "Campos incompletos"})
+    }
+
     return res.status(201).send(user);
 
+
   } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
+    console.error(error);
+    if (error.code === 11000) {
+      res.status(400).send({ error: 'El email ya está registrado.' });
+    } else {
+      res.status(500).send({ error: 'Error interno del servidor' });
+    }
   }
 });
 
 // PUT /api/user/:id
-router.put("/api/user/:id",  async (req, res) => {
+/*router.put("/api/user/:id",  async (req, res) => {
   // #swagger.tags = ['Usuario']
   try {
     const userId = req.params.id;
@@ -61,7 +96,41 @@ router.put("/api/user/:id",  async (req, res) => {
     return res.status(500).send(error);
   }
 });
+*/
 
+
+// PUT /api/user/:id
+router.put("/api/user/:id", async (req, res) => {
+  // #swagger.tags = ['Usuario']
+  try {
+    const userId = req.params.id;
+    // const updatedUser = req.body;
+    const { firstname, lastname, email, domicilio, celular, documento, rol, area } = req.body
+
+    const updatedUser = {
+      firstname,
+      lastname,
+      email,
+      domicilio,
+      celular,
+      documento,
+      rol,
+      area,
+    }
+
+    
+   
+    // Actualizar usuario por ID
+    const user = await userService.update(userId, updatedUser);
+
+   
+
+    return res.status(200).send(user);
+  } catch (error) {
+    console.error("Error al actualizar usuario:", error);
+    return res.status(500).send({ error: "Error interno del servidor" });
+  }
+});
 // DELETE /api/user/:id
 router.delete("/api/user/:id", async (req, res) => {
   // #swagger.tags = ['Usuario']
